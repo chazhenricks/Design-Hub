df1d46cd464b9640ce42c9f1dbe55f639f4ca91b
chazhenricks <chazhenricks@gmail.com>
2017-09-17 12:47:34 -0700

    returning different views on login works

--- designhub/Controllers/HomeController.cs
+++ designhub/Controllers/HomeController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using designhub.Models;
+
 
 namespace designhub.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index(object Test)
+        private readonly UserManager<ApplicationUser> _userManager;
 
-            
+        public HomeController(UserManager<ApplicationUser> userManager)
         {
+          
+            _userManager = userManager;
+        }
+
+        // This task retrieves the currently authenticated user
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+
+
+        public IActionResult Index(object Test)     
+        {
+            var user = GetCurrentUserAsync();
+            if (User.Identity.IsAuthenticated)
+            {
+                return View("LoggedInView");
+            }
             return View();
         }
 

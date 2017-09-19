7a5c423851d9538e58a8a8ef844909dd2973faa1
chazhenricks <chazhenricks@gmail.com>
2017-09-19 12:12:47 -0700

    adding an abstraction view for empty projects

--- designhub/Controllers/DocumentGroupsController.cs
+++ designhub/Controllers/DocumentGroupsController.cs
@@ -14,21 +14,34 @@ namespace designhub.Controllers
 {
     public class DocumentGroupsController : Controller
     {
+        // *****************************
+        // *****************************
+        // CONTEXT SETUP
+        // *****************************
+        // *****************************
+
+
         private readonly ApplicationDbContext _context;
 
         public DocumentGroupsController(ApplicationDbContext context)
         {
             _context = context;    
         }
+        // *****************************
+        // *****************************
+        // GET CALLS
+        // *****************************
+        // *****************************
 
         // GET: DocumentGroups
+        //id refers to ProjectID
         public async Task<IActionResult> Index(int id)
         {
-
+            //creates new instance of the DocumentGroupsList
             DocumentGroupsList viewModel = new DocumentGroupsList();
 
          
-
+            //grabs each document group for a given project id
             var documentGroups = await (
                 from d in _context.DocumentGroup
                 from pd in _context.ProjectDocumentGroup
@@ -38,8 +51,10 @@ namespace designhub.Controllers
                 .OrderByDescending(dg => dg.DateCreated)
                 .ToListAsync();
 
+            //grabs an instance of the project
             var project = await _context.Project.SingleAsync(p => p.ProjectID == id);
 
+            //creates a new list of documents 
             List<Document> documents = new List<Document>();
 
 
@@ -49,30 +64,40 @@ namespace designhub.Controllers
                 return NotFound();
             }
 
+            //if there are document groups present
             if (documentGroups.Count > 0)
             {
+                //loop through them
                 foreach (var dg in documentGroups)
                 {
+                    //grab any documents associated with a docuemtn group and add it to a list
                     var dgDocuments = await _context.Document
                                     .Where(d => d.DocumentGroupID == dg.DocumentGroupID)
                                     .OrderByDescending(d => d.DateCreated)
                                     .ToListAsync();
+                    //go through that list and push to our overall document list
                     foreach (Document doc in dgDocuments)
                     {
                         documents.Add(doc);
                     }
                 }
-               
+               //builds the viewModel with a project, list of document groups, and list of documents and returns index view with that viewModel
                 viewModel.DocumentGroups = documentGroups;
                 viewModel.Project = project;
                 viewModel.Documents = documents;
                 return View(viewModel);
             }
-        
-            return RedirectToAction("Create", new {id = id } );
+
+            //if there are no document groups for a given project ID, redirect to create a new document group 
+            EmptyProjectViewModel viewModel = new EmptyProjectViewModel();
+            return View("EmptyProject", viewModel);
 
             
         }
+
+       // *****************************
+       // POTENTIALLY  NOT USED
+       // ******************************
 
         // GET: DocumentGroups/Details/5
         public async Task<IActionResult> Details(int? id)
@@ -96,15 +121,25 @@ namespace designhub.Controllers
             return View(documentGroup);
         }
 
+
+
         // GET: DocumentGroups/Create
+        //id refers to Project ID
         public IActionResult Create(int id)
         {
+            //Creates a blank document group, but passes Project ID along with it
             CreateDocumentGroupViewModel viewModel = new CreateDocumentGroupViewModel();
             viewModel.ProjectID = id;
             viewModel.DocumentGroup = new DocumentGroup();
 
             return View(viewModel);
         }
+
+        // *****************************
+        // *****************************
+        // POST METHODS
+        // *****************************
+        // *****************************
 
         // POST: DocumentGroups/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
@@ -113,28 +148,35 @@ namespace designhub.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create( CreateDocumentGroupViewModel viewModel)
         {
+            //sets date created to current date
             viewModel.DocumentGroup.DateCreated = DateTime.Now;
             if (ModelState.IsValid)
             {
-
+                //sets up ProjectDocumentGroup and assisns ProjectID to it to update as soon as new document group gets created.
                 ProjectDocumentGroup newProjectDocumentGroup = new ProjectDocumentGroup();
                 newProjectDocumentGroup.ProjectID = viewModel.ProjectID;
 
-
-
+                //Adds new DocumentGroup to DB
                 _context.Add(viewModel.DocumentGroup);
                 await _context.SaveChangesAsync();
 
+                //Assigns the DocumentGroupID of the newly added DocumentGroup to the new ProjectDocumentGroup model and updates the ProjectDocumentGroup join table
                 newProjectDocumentGroup.DocumentGroupID = viewModel.DocumentGroup.DocumentGroupID;
                 _context.ProjectDocumentGroup.Add(newProjectDocumentGroup);
                 await _context.SaveChangesAsync();
 
+                //Once added it redirects user to the index view for current Project
                 return RedirectToAction("Index", new { id = viewModel.ProjectID });
 
             }
-            return RedirectToAction("Index");
+            //If model validation fails will redirect back to the index view for current project
+            return RedirectToAction("Index", new { id = viewModel.ProjectID});
         }
 
+
+        // *****************************
+        // POTENTIALLY  NOT USED
+        // ******************************
         // GET: DocumentGroups/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -186,6 +228,12 @@ namespace designhub.Controllers
             return View(documentGroup);
         }
 
+        // *****************************
+        // *****************************
+        // Delete Methods
+        // *****************************
+        // *****************************
+
         // GET: DocumentGroups/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
@@ -194,6 +242,7 @@ namespace designhub.Controllers
                 return NotFound();
             }
 
+            //grabs current documentGroup to delete
             var documentGroup = await _context.DocumentGroup
                 .SingleOrDefaultAsync(m => m.DocumentGroupID == id);
             if (documentGroup == null)
@@ -201,18 +250,39 @@ namespace designhub.Controllers
                 return NotFound();
             }
 
-            return View(documentGroup);
+            //creates a new instance of DeleteDocumentGroup model, that contains a DocumentGroup and Project ID
+            DeleteDocumentGroup viewModel = new DeleteDocumentGroup();
+
+
+            //grabs Project by dipping into ProjectDocumentGroup join table
+            var projectDocGroupList = await (
+                   from pdg in _context.ProjectDocumentGroup
+                   where pdg.DocumentGroupID == documentGroup.DocumentGroupID
+                   select pdg)
+                   .ToListAsync();
+
+            //grabs first return
+            var pg = projectDocGroupList.First();
+
+            viewModel.DocumentGroup = documentGroup;
+            viewModel.ProjectID = pg.ProjectID;
+
+            //Returns delete view with viewModel
+            return View(viewModel);
         }
 
+        //Actual delete method
+        //id is DocumentGroupID and ProjectID is ProjectID
         // POST: DocumentGroups/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> DeleteConfirmed(int id, int ProjectID)
         {
             var documentGroup = await _context.DocumentGroup.SingleOrDefaultAsync(m => m.DocumentGroupID == id);
             _context.DocumentGroup.Remove(documentGroup);
             await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
+            //after delete, redirects to the index page for current Project
+            return RedirectToAction("Index" , new { id = ProjectID});
         }
 
         private bool DocumentGroupExists(int id)
